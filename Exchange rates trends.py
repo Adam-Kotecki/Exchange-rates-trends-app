b44a8cc4e23d8d0777494d1b1d16cea5680b3bcc
Adam-Kotecki <a.kotecki11@gmail.com>
2022-12-21 23:29:37 +0100

    GUI implementation

--- Exchange rates trends.py
+++ Exchange rates trends.py
@@ -1,6 +1,52 @@
 import requests
 import json
+import tkinter as tk
+from tkinter import ttk
+from tkinter import Entry
+from tkinter.messagebox import showinfo
+from PIL import Image, ImageTk
+from tkcalendar import Calendar, DateEntry
 
+root = tk.Tk()
+
+root.geometry('400x500')
+#root.resizable(False, False)
+root.title('Exchange rates trends app')
+
+label2 = ttk.Label(text="Select currency:")
+label2.place(x=5 , y=120)
+
+selected_currency = tk.StringVar()
+currency = ttk.Combobox(root, textvariable=selected_currency)
+currency['values'] = ('EUR', 'USD', 'CZK', 'JPY', 'AUD', 'GBP', 'UAH')
+currency['state'] = 'readonly'
+currency.place(x=5 , y=140)
+
+label3 = ttk.Label(text="Start date:")
+label3.place(x=5 , y=170)
+
+#cal = Calendar(root)
+cal = DateEntry(root, width= 16, background= "green", foreground= "white",bd=2)
+cal.place(x=5 , y=190)
+
+label4 = ttk.Label(text="End date:")
+label4.place(x=5 , y=220)
+
+cal2 = DateEntry(root, width= 16, background= "green", foreground= "white",bd=2)
+cal2.place(x=5 , y=240)
+
+label4 = ttk.Label(text="Monotonicity of the sequence:")
+label4.place(x=5 , y=270)
+
+monotonicity = tk.StringVar()
+mono = ttk.Combobox(root, textvariable=monotonicity)
+mono['values'] = ('non-decreasing', 'non-increasing')
+mono['state'] = 'readonly'
+mono.place(x=5 , y=290)
+
+root.mainloop()
+
+"""
 
 currency = input("Please provide currency: ")
 start = str(input("Please provide start date (YYYY-MM-DD): "))
@@ -70,5 +116,5 @@ if mono == "non-decreasing":
 else:
     non_increasing_longest_sequence(exchange_rates)
     
-    
+"""
     
